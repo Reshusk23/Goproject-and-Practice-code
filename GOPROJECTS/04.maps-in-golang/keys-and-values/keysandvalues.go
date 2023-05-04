efdaa5e12b3096cee8c2df8b9b151b106d0f047f
Reshma Shaik <126868707+Reshusk23@users.noreply.github.com>
2023-05-04 08:04:39 -0700

    updated
    new code

--- GOPROJECTS/04.maps-in-golang/keys-and-values/keysandvalues.go
+++ GOPROJECTS/04.maps-in-golang/keys-and-values/keysandvalues.go
@@ -11,6 +11,24 @@ func main() {
 	match["bob"] = "Emma"
 	fmt.Println(match)*/
 
+	/*bob := map[string]string{
+		"Bob":   "Sana",
+		"david": "Sophia",
+		"bob":   "Emma",
+	}
+
+	key := make([]string, 0)
+	for keys := range bob {
+		key = append(key, keys)
+	}
+	fmt.Printf("%q", key)*/
+
+	bob := map[string]int{
+		"foo": 1,
+		"bar": 2,
+		"baz": 3,
+	}
+
 	key := []string{}
 	for keys := range bob {
 		key = append(key, keys)

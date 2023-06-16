745de88089116733e8e761cafde4d908894e7a3a
Reshma Shaik <126868707+Reshusk23@users.noreply.github.com>
2023-06-16 22:38:09 +0530

    commit

---
+++ GOPROJECTS/08.packages_in_golang/058-standard_library_packages/standard-library-packages.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+//import "math"
+//import "expvar"
+//import "flag"
+
+func main() {
+	fmt.Println("hello")
+}
---
+++ GOPROJECTS/08.packages_in_golang/059-installing_packages/installing-packages.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	//"github.com/gorilla/mux"
+	//"io"
+	//"net/http"
+)
